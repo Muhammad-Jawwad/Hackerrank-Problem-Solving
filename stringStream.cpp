850ef25b1540c4f1807279be44d88b8290217093
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2023-02-20 23:16:21 +0500

    Create stringStream.cpp

---
+++ stringStream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> i;
+    while(ss){
+        char ch;
+        int a;
+        ss >> a >> ch; 
+        i.push_back(a);
+    }
+    return i;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
